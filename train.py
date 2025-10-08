cf74bd0d95aa3c0046022c5e92ddee53d1be6b7e
Adam McCaughan <amccaugh@users.noreply.github.com>
2025-10-08 17:01:32 -0600

    Begin basic data gathering

--- train.py
+++ train.py
@@ -16,44 +16,45 @@ from flax import serialization
 from flax.training import train_state
 
 from model import GPTConfig, GPT
+import jax.numpy as jnp
 
 
 # -----------------------------------------------------------------------------
-# default config values designed to train a gpt2 (124M) on OpenWebText
-# I/O
-out_dir = 'out'
-eval_interval = 2000
-log_interval = 1
-eval_iters = 200
+# # default config values designed to train a gpt2 (124M) on OpenWebText
+# # I/O
+# out_dir = 'out'
+# eval_interval = 2000
+# log_interval = 1
+# eval_iters = 200
 eval_only = False # if True, script exits right after the first eval
-always_save_checkpoint = True # if True, always save a checkpoint after each eval
+# always_save_checkpoint = True # if True, always save a checkpoint after each eval
 init_from = 'scratch' # 'scratch' or 'resume' or 'gpt2*'
-# data
-dataset = 'openwebtext'
-gradient_accumulation_steps = 5 * 8 # used to simulate larger batch sizes
-batch_size = 12 # if gradient_accumulation_steps > 1, this is the micro-batch size
-block_size = 1024
-tokens_per_iter = gradient_accumulation_steps * batch_size * block_size
-print(f"tokens per iteration will be: {tokens_per_iter:,}")
-# model
-n_layer = 12
-n_head = 12
-n_embd = 768
-dropout = 0.0 # for pretraining 0 is good, for finetuning try 0.1+
-use_bias = False # do we use bias inside LayerNorm and Linear layers?
-# adamw optimizer
-learning_rate = 6e-4 # max learning rate
-max_iters = 600000 # total number of training iterations
+# # data
+# dataset = 'openwebtext'
+# gradient_accumulation_steps = 5 * 8 # used to simulate larger batch sizes
+# batch_size = 12 # if gradient_accumulation_steps > 1, this is the micro-batch size
+# block_size = 1024
+# tokens_per_iter = gradient_accumulation_steps * batch_size * block_size
+# print(f"tokens per iteration will be: {tokens_per_iter:,}")
+# # model
+# n_layer = 12
+# n_head = 12
+# n_embd = 768
+# dropout = 0.0 # for pretraining 0 is good, for finetuning try 0.1+
+use_bias = True # do we use bias inside LayerNorm and Linear layers?
+# # adamw optimizer
+# learning_rate = 6e-4 # max learning rate
+# max_iters = 600000 # total number of training iterations
 weight_decay = 1e-1
 beta1 = 0.9
-beta2 = 0.95
+# beta2 = 0.95
 grad_clip = 1.0 # clip gradients at this value, or disable if == 0.0
-# learning rate decay settings
+# # learning rate decay settings
 decay_lr = True # whether to decay the learning rate
-warmup_iters = 2000 # how many steps to warm up for
-lr_decay_iters = 600000 # should be ~= max_iters per Chinchilla
-min_lr = 6e-5 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
-max_checkpoints_to_keep = 5
+# warmup_iters = 2000 # how many steps to warm up for
+# lr_decay_iters = 600000 # should be ~= max_iters per Chinchilla
+# min_lr = 6e-5 # minimum learning rate, should be ~= learning_rate/10 per Chinchilla
+# max_checkpoints_to_keep = 5
 
 
 
@@ -150,27 +151,27 @@ if init_from == 'scratch':
     model = GPT(gptconf)
     state = model.configure_state(**config)
     params = state.params
-elif init_from == 'resume':
-    print(f"Resuming training from {out_dir}")
-    # resume training from a checkpoint.
-    last_step = checkpoint_manager.latest_step()
-    assert last_step is not None, "No checkpoint available!"
-    checkpoint = checkpoint_manager.restore(last_step)
-    checkpoint_model_args = checkpoint['model_args']
-    # force these config attributes to be equal otherwise we can't even resume training
-    # the rest of the attributes (e.g. dropout) can stay as desired from command line
-    for k in ['n_layer', 'n_head', 'n_embd', 'block_size', 'use_bias', 'vocab_size']:
-        model_args[k] = checkpoint_model_args[k]
-    # create the model
-    gptconf = GPTConfig(**model_args)
-    model = GPT(gptconf)
-    # load state
-    empty_state = jax.eval_shape(lambda: model.configure_state(**config))
-    state = serialization.from_state_dict(empty_state, checkpoint['state'])
-    iter_num = checkpoint['iter_num']
-    best_val_loss = checkpoint['best_val_loss']
-elif init_from.startswith('gpt2'):
-    raise NotImplementedError("Pretrained model loading is not implemented.")
+# elif init_from == 'resume':
+#     print(f"Resuming training from {out_dir}")
+#     # resume training from a checkpoint.
+#     last_step = checkpoint_manager.latest_step()
+#     assert last_step is not None, "No checkpoint available!"
+#     checkpoint = checkpoint_manager.restore(last_step)
+#     checkpoint_model_args = checkpoint['model_args']
+#     # force these config attributes to be equal otherwise we can't even resume training
+#     # the rest of the attributes (e.g. dropout) can stay as desired from command line
+#     for k in ['n_layer', 'n_head', 'n_embd', 'block_size', 'use_bias', 'vocab_size']:
+#         model_args[k] = checkpoint_model_args[k]
+#     # create the model
+#     gptconf = GPTConfig(**model_args)
+#     model = GPT(gptconf)
+#     # load state
+#     empty_state = jax.eval_shape(lambda: model.configure_state(**config))
+#     state = serialization.from_state_dict(empty_state, checkpoint['state'])
+#     iter_num = checkpoint['iter_num']
+#     best_val_loss = checkpoint['best_val_loss']
+# elif init_from.startswith('gpt2'):
+#     raise NotImplementedError("Pretrained model loading is not implemented.")
 # crop down the model block size if desired, using model surgery
 if block_size < model.config.block_size:
     model.crop_block_size(block_size)
@@ -206,9 +207,17 @@ def train_step(state: train_state.TrainState, batch):
     grad_fn = jax.value_and_grad(loss_fn)
     loss, grad = grad_fn(state.params)
     state = state.apply_gradients(grads=grad)
-    return loss, state
+    return loss, state, grad
 
 # -----------------------------------------------------------------------------
+
+# Record biases for deltavec analysis
+recorded_biases = []
+is_bias = jax.tree_util.tree_map_with_path(lambda path, leaf: path[-1].key == 'bias', state.params)
+
+
+
+
 # training loop
 t0 = time.time()
 while True:
@@ -233,7 +242,7 @@ while True:
     if iter_num == 0 and eval_only:
         break
     
-    loss, state = train_step(state, get_batch('train'))
+    loss, state, grad = train_step(state, get_batch('train'))
 
     # timing and logging
     t1 = time.time()
@@ -244,10 +253,53 @@ while True:
         print(f"iter {iter_num}: loss {lossf:.4f}, time {dt*1000:.2f}ms")
     iter_num += 1
 
+
+
     # termination conditions
     if iter_num > max_iters:
         break
 
-# checkpoint_manager.close()
+    # Record biases
+    recorded_biases.append(jax.tree.map(lambda ib,g: g if ib else None, is_bias, grad))
+
+    # Record the bias gradients for a few layers
+    # grad['h_0']['attn']['c_attn']['bias']
+    # grad['h_0']['attn']['c_proj']['bias']
+    # grad['h_0']['ln_1']['bias']
+    # grad['h_0']['ln_2']['bias']
+    # grad['h_0']['mlp']['c_proj']['bias']
+    # grad['h_0']['mlp']['c_fc']['bias']
+
+
+#%% Perform deltavec analysis
+
+
+from matplotlib import pyplot as plt
+
+def tree_stack(list_of_trees):
+  """Convert a list of trees of identical structure into a single tree of lists.
+  From JAX tutorial https://jax.readthedocs.io/en/latest/jax-101/05.1-pytrees.html"""
+  return jax.tree.map(lambda *xs: jnp.stack(xs), *list_of_trees)
+
+stacked_biases = tree_stack(recorded_biases)
+
+
+plt.pcolor(stacked_biases['h_0']['attn']['c_attn']['bias'], shading='nearest')
+plt.show()
+plt.pcolor(stacked_biases['h_0']['attn']['c_proj']['bias'], shading='nearest')
+plt.show()
+plt.pcolor(stacked_biases['h_0']['ln_1']['bias'], shading='nearest')
+plt.show()
+plt.pcolor(stacked_biases['h_0']['ln_2']['bias'], shading='nearest')
+plt.show()
+plt.pcolor(stacked_biases['h_0']['mlp']['c_proj']['bias'], shading='nearest')
+plt.show()
+plt.pcolor(stacked_biases['h_0']['mlp']['c_fc']['bias'], shading='nearest')
+plt.show()
+plt.pcolor(stacked_biases['h_0']['mlp']['c_fc']['bias'], shading='nearest')
+plt.show()
+# plt.imshow(stacked_biases['h_0']['attn']['c_attn']['bias'])
+plt.colorbar()
+plt.show()
 
 # %%
